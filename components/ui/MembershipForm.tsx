dbf7eb443490f62362b4d53b3c2ce5f39604764c
keeper-app <keeper@gmail.com>
2026-02-25 14:05:08 +0500

    first commit

--- components/ui/MembershipForm.tsx
+++ components/ui/MembershipForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useRef } from 'react';
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 import ReCAPTCHA from 'react-google-recaptcha';
 import { FaCheckCircle } from 'react-icons/fa';
 
